b65d8f7186310eb19babb7198e5ee087f2612380
Connor <sharsnik@gmail.com>
2020-06-06 22:09:47 -0400

    Trying Xdummy

--- unity/start.sh
+++ unity/start.sh
@@ -3,6 +3,8 @@ set -e
 
 Xvfb :100 +extension Composite +extension RANDR +extension RENDER +extension GLX -screen 0 100x100x24 &
 
+export DISPLAY=:100
 echo "=========== XVFB started ========"
+glxinfo
 
-python3 /seed_rl/gcp/run.py "$@"
+#python3 /seed_rl/gcp/run.py "$@"
