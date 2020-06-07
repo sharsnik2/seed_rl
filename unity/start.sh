7982654e02918fe06b396ffabd300a90ff2f6eb1
Connor <sharsnik@gmail.com>
2020-06-06 20:05:19 -0400

    Lots of fail

--- unity/start.sh
+++ unity/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e
 
 Xvfb :100 +extension Composite +extension RANDR +extension RENDER +extension GLX -screen 0 100x100x24 &
 
-python3 ../gcp/run.py
+export DISPLAY=:100
+
+python3 /seed_rl/gcp/run.py "$@"
