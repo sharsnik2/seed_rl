6a36f2c5d4f4804d2891cbc59bf54ec02a974b5e
Connor <sharsnik@gmail.com>
2020-06-06 20:46:24 -0400

    Docker from football

--- unity/start.sh
+++ unity/start.sh
@@ -3,4 +3,6 @@ set -e
 
 Xvfb :100 +extension Composite +extension RANDR +extension RENDER +extension GLX -screen 0 100x100x24 &
 
+echo "=========== XVFB started ========"
+
 python3 /seed_rl/gcp/run.py "$@"
